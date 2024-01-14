3ab9e12dff4cb6a0f792d4008fc497b49598ff4d
mihaf <fahimeh2067@gmail.com>
2024-01-14 12:42:34 +0330

    Create Component

--- src/index.js
+++ src/index.js
@@ -1,20 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';  
 
-
-let elem=<h1>سلام دوستان عزیز</h1>
-
-const tick=()=>{
-  const item=(
-    <div>
-        <h1>میهاف سلام</h1>
-        <h2>
+class Timer extends React.Component{
+  render(){
+    return(
+      <h2>
            
            it is  {new Date().toLocaleTimeString()=="12:07:15 PM"?clearInterval : new Date().toLocaleTimeString()}
         </h2>
+    )
+  }
+}
+class Hello extends React.Component{
+  render(){
+    return(
+      <h1>سلام دوستان من</h1>
+    )
+  }
+}
+
+
+class App extends React.Component{
+render(){
+return ( 
+    <div>
+        <Hello/>
+         <Timer/>
     </div>
-  )
-  ReactDOM.render(item,document.getElementById('root'));
+  ) 
+}
+  
+}
+
+//let elem=new App()elem.render()
+const tick=()=>{ 
+  ReactDOM.render(<App/>,document.getElementById('root'));
 }
 
 setInterval(() => {
