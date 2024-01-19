771138ee23c8af04890d6f1e6f79c8eb707467b7
mihaf <fahimeh2067@gmail.com>
2024-01-19 21:09:56 +0330

    takmil sabt zaman -spread operator in es6

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
-import React,{useState}  from "react";
+import React,{useState,useEffect}  from "react";
 //import ReactDOM  from "react-dom";
 import Hello from "./Hello";
 import Timer from "./Timer";
 import './style.css'
+import TimeList from "./TimeList";
 
 
 const App=()=>{
      const [title , setTitle]=useState("سلام دوستان عزیز");
-     const handleSetTitle=()=>{
-      setTitle("به کد یاد خوش امدید")
+     const [isLight , setIsLight]=useState(false);
+     const [timeArr,setTimeArr]=useState(["00 :05 : 12","01 : 15 :10","12 : 15 :10"]);
+      const handleSetTitle=()=>{
+        if (isLight)
+        { setTitle("به کد یاد خوش امدید")}
+        else
+        {  setTitle("سلام دوستان عزیزم")}
+      }
+     const handleSetIsLight=()=>{
+      setIsLight(!isLight)
+      //console.log(isLight);
      }
+     useEffect(()=>{
+      handleSetTitle();
+      return()=>
+      {
+
+      }
+     },[isLight])
      return ( 
-               <div className='main'>
+      
+               <div className='main' style={{background:isLight?"white":"black"}}>
                     <Hello title={title}/>
-                    <Timer handleSetTitle={handleSetTitle}/>
+                    <Timer 
+                          isLight={isLight} 
+                          handleSetIsLight={handleSetIsLight} 
+                          handleSetTitle={handleSetTitle}
+                          timeArr={timeArr}
+                          setTimeArr={setTimeArr}
+                    />
+                    
                </div>
              )
 }
--- src/Timer.js
+++ src/Timer.js
@@ -1,71 +1,84 @@
-import React  from "react";
+import React from "react";
 //import ReactDOM  from "react-dom";
 import './style.css'
+import TimeList from "./TimeList";
 
 var interval;
-class Timer extends React.Component{
-    constructor(){
-      super();
-      this.state={
-        hour :0,
-        minute:0,
-        second:0,
-        isStart:false 
-      }
+class Timer extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      hour: 0,
+      minute: 0,
+      second: 0,
+      isStart: false
     }
-    startInterval=()=>{
-      if (this.state.isStart===false)
-      {
-        this.setState({isStart:true})
-      interval=setInterval(() => {
-       this.setState({second:this.state.second+1}) 
-       if (this.state.second===60)
-          {
-            this.setState({
-              second:0,
-              minute:this.state.minute+1
-              }) 
-          }    
-          if (this.state.minute===60)
-          {
-            this.setState({
-              minute:0,
-              hour:this.state.hour+1
-              }) 
-          }    
+  }
+  startInterval = () => {
+    if (this.state.isStart === false) {
+      this.setState({ isStart: true })
+      interval = setInterval(() => {
+        this.setState({ second: this.state.second + 1 })
+        if (this.state.second === 60) {
+          this.setState({
+            second: 0,
+            minute: this.state.minute + 1
+          })
+        }
+        if (this.state.minute === 60) {
+          this.setState({
+            minute: 0,
+            hour: this.state.hour + 1
+          })
+        }
       }, 1000);
-    }
-    }
-
-    resetInterval=()=>{
-      this.setState({hour:0,minute:0,second:0})
-      this.stopInterval()
-    }
-    stopInterval=()=>{
-      this.setState({isStart:false})
-      clearInterval(interval)
-    }
-
-    render(){
-      let h=this.state.hour.toString().padStart(2,'0')
-      let m=this.state.minute.toString().padStart(2,'0')
-      let s=this.state.second.toString().padStart(2,'0')
-      return(
-         <>
-         <h2 className='timer'>
-               {`${h} : ${m} : ${s}`}
-          </h2>
-          {/* <button onClick={this.props.handleSetTitle}>change</button>*/}
-
-           <div className="button_box">
-               <spam className="action_button start_burtton" onClick={this.startInterval} >Start</spam>
-               <spam className="action_button stop_burtton" onClick={this.stopInterval}>Stop</spam>
-               <spam className="action_button reset_burtton" onClick={this.resetInterval}>Reset</spam>
-               <spam className="action_button reset_burtton" onClick={this.props.handleSetTitle}>Title</spam>
-           </div>
-          </>
-      )
     }
   }
 
-  export default Timer;
+  resetInterval = () => {
+    this.setState({ hour: 0, minute: 0, second: 0 })
+    this.stopInterval()
+  }
+  stopInterval = () => {
+    this.setState({ isStart: false })
+    clearInterval(interval)
+  }
+  HandleSaveTime=()=>{
+    let h = this.state.hour.toString().padStart(2, '0')
+    let m = this.state.minute.toString().padStart(2, '0')
+    let s = this.state.second.toString().padStart(2, '0')
+    let newTime=`${h} : ${m} : ${s}`;
+    this.props.setTimeArr([...this.props.timeArr,newTime]);
+  }
+  render() {
+    let h = this.state.hour.toString().padStart(2, '0')
+    let m = this.state.minute.toString().padStart(2, '0')
+    let s = this.state.second.toString().padStart(2, '0')
+    return (
+      <>
+        <h2 className='timer' onClick={this.HandleSaveTime}>
+          {`${h} : ${m} : ${s}`}
+        </h2>
+        {/* <button onClick={this.props.handleSetTitle}>change</button>*/}
+
+        <div className="button_box">
+          <spam className="action_button start_burtton" onClick={this.startInterval} >Start</spam>
+          <spam className="action_button stop_burtton" onClick={this.stopInterval}>Stop</spam>
+          <spam className="action_button reset_burtton" onClick={this.resetInterval}>Reset</spam>
+          <spam className="action_button reset_burtton"
+            style={{
+              background: this.props.isLight ? "black" : "white"
+              , color: this.props.isLight ? "white" : "black"
+            }} onClick={this.props.handleSetIsLight}>
+            {this.props.isLight ? "Dark" : "Light"}
+          </spam> 
+        </div>
+        <TimeList>
+               {this.props.timeArr}
+          </TimeList>
+      </>
+    )
+  }
+}
+
+export default Timer;
